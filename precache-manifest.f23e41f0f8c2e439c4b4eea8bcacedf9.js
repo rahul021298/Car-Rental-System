03d45d5c91fe2dd9e05b3f65716725fe7565643c
rahul0212 <rahulmakhija02@gmail.com>
2020-05-03 12:58:34 +0530

    Updates

---
+++ precache-manifest.f23e41f0f8c2e439c4b4eea8bcacedf9.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fc68abea80ea3b0b7d3ae9eb6f0c549",
+    "url": "/Car-Rental-System/index.html"
+  },
+  {
+    "revision": "312ff3475125ec4dc496",
+    "url": "/Car-Rental-System/static/css/main.66cb5fad.chunk.css"
+  },
+  {
+    "revision": "ec4a7b95366a2db36f9f",
+    "url": "/Car-Rental-System/static/js/2.8f0fb33e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Car-Rental-System/static/js/2.8f0fb33e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "312ff3475125ec4dc496",
+    "url": "/Car-Rental-System/static/js/main.ba192abd.chunk.js"
+  },
+  {
+    "revision": "797c039b5439cb639a83",
+    "url": "/Car-Rental-System/static/js/runtime-main.40b98494.js"
+  },
+  {
+    "revision": "d61546cda03af82f672458cb776c67a1",
+    "url": "/Car-Rental-System/static/media/boatImage.d61546cd.jpg"
+  },
+  {
+    "revision": "c4305b911f6fea747e16a9f6a3e1a856",
+    "url": "/Car-Rental-System/static/media/car.c4305b91.png"
+  },
+  {
+    "revision": "1048970fd956028d51364caf8f3b6964",
+    "url": "/Car-Rental-System/static/media/car1.1048970f.jpeg"
+  },
+  {
+    "revision": "c3bff3b8240d2f3cea171958931217fa",
+    "url": "/Car-Rental-System/static/media/car2.c3bff3b8.jpeg"
+  }
+]);
